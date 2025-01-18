2fefb1f826639d19304ab07ad824c39f5be29afa
Your Name <laurentiu.lahman@eestec.ro>
2025-01-18 23:32:58 +0200

    Added weather API key to .env file Added weather API and made it to respond Added search functionality to search for weather by city name

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './weather.css'
 import search from '../assets/search.png'
 import clear from '../assets/clear.png'
@@ -10,32 +10,104 @@ import snow from '../assets/snow.png'
 import wind from '../assets/wind.png'
 
 
+
+
+
 const Weather = () => {
+
+    const inputRef = useRef();
+
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear,
+        "01n": clear,
+        "02d": cloud,
+        "02n": cloud,
+        "03d": cloud,
+        "03n": cloud,
+        "04d": drizzle,
+        "04n": drizzle,
+        "09d": rain,
+        "09n": rain,
+        "10d": rain,
+        "10n": rain,
+        "13d": snow,
+        "13n": snow,
+    }
+
+    const getWeather = async (city) => {
+
+        if(city === "") {
+            alert("Coaie ej prost? Pune un oras");
+            return;
+        }
+        try {
+
+            const apiKey = import.meta.env.VITE_APP_ID;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if(!response.ok) {
+                alert(data.message);
+                return;
+            }
+
+            const icon = allIcons[data.weather[0].icon] || clear;
+            console.log(data);
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
+        } catch (error) {
+            setWeatherData(false);
+            console.error("Ai belit pla")
+        }
+    }
+
+    useEffect(() => {
+        getWeather("London");
+    },[])
+
+
   return (
+
+    
+    
+
+
     <div className="weather">
         <div className="search-box">
-            <input type="text" placeholder="Search"/>
-            <img src={search} alt="" />
+            <input ref={inputRef} type="text" placeholder="Search"/>
+            <img src={search} alt="" onClick={() => getWeather(inputRef.current.value)}/>
         </div>
-        <img src={clear} alt="" className="weather-icon"/>
-        <p className="temperature">16°C</p>
-        <p className="location">London</p>
+        {weatherData ? <>
+            <img src={weatherData.icon} alt="" className="weather-icon"/>
+        <p className="temperature">{weatherData.temperature}°C</p>
+        <p className="location">{weatherData.location}</p>
         <div className="weather-data">
             <div className="col">
                 <img src={humidity} alt="" />
                 <div>
-                    <p>91 %</p>
+                    <p>{weatherData.humidity} %</p>
                     <span>Humidity</span>
                 </div>
             </div>
             <div className="col">
                 <img src={wind} alt="" />
                 <div>
-                    <p>3.6 Km/h</p>
+                    <p>{weatherData.windSpeed} Km/h</p>
                     <span>Wind Speed</span>
                 </div>
             </div>
         </div>
+        </> : <></>}
+        
     </div>
   );
 }
